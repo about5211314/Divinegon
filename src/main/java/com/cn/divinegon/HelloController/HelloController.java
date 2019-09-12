b1b17ccb9a1704ba773d70ca460bcb0b44be99cc
space <45583285@qq.com>
2019-09-12 13:16:05 +0800

    first up

---
+++ src/main/java/com/cn/divinegon/HelloController/HelloController.java
@@ -0,0 +1,19 @@
+package com.cn.divinegon.HelloController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试控制器
+ *
+ * @author: @沈立
+ * @create: 2019-09-12-下午 16:46
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
