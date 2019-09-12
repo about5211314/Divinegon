fa1310f6ef5db94cfb9ea1e97b024e94bfd7afe8
space <45583285@qq.com>
2019-09-12 12:51:30 +0800

    first up

---
+++ src/main/java/com/cn/divinegon/DivinegonApplication.java
@@ -0,0 +1,13 @@
+package com.cn.divinegon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivinegonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivinegonApplication.class, args);
+    }
+
+}
