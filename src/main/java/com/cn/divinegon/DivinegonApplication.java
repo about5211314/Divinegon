05700b2af64d2e9acd1dfc131a4f1a120fa415d7
zhuwei4j <982212965@qq.com>
2019-09-20 11:04:31 +0800

    add mybatis

--- src/main/java/com/cn/divinegon/DivinegonApplication.java
+++ src/main/java/com/cn/divinegon/DivinegonApplication.java
@@ -1,9 +1,14 @@
 package com.cn.divinegon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableAutoConfiguration
+@MapperScan("com.cuit.springboot.gdao")
 public class DivinegonApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/cn/divinegon/HelloController/HelloController.java
+++ src/main/java/com/cn/divinegon/HelloController/HelloController.java
@@ -1,7 +1,14 @@
 package com.cn.divinegon.HelloController;
 
+import com.cuit.springboot.gdao.StudentMapper;
+import com.cuit.springboot.gentry.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * 测试控制器
@@ -9,11 +16,40 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: @沈立
  * @create: 2019-09-12-下午 16:46
  */
-@RestController
+@Controller
 public class HelloController {
+    @Autowired
+    private StudentMapper studentMapper;
+    private List<Student> listStudent = null;
 
-    @RequestMapping("/hello")
+    @RequestMapping("/index")
     public String hello() {
-        return "Hello Spring Boot!";
+        return "index";
+    }
+
+    @RequestMapping("/userList")
+    public String userList(ModelMap map) {
+        listStudent = studentMapper.selectAll();
+        map.addAttribute("listStudent", listStudent);
+        return "user/user";
+    }
+
+    @RequestMapping("/userEdit")
+    public String userEdit(int id, ModelMap map) {
+        Student obj = studentMapper.selectByPrimaryKey(id);
+        map.addAttribute("userName", obj.getUser_name());
+        map.addAttribute("classes", obj.getClass_es());
+        return "user/userEdit";
+    }
+
+    @RequestMapping("/userNew")
+    public String userNew() {
+        return "user/userNew";
+    }
+
+    @RequestMapping("/add")
+    public String add(@RequestParam("ids") String ids) {
+        System.out.println(ids);
+        return "success";
     }
 }
